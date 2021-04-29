c3cbf4213670d28029e53a87be4c1d77f5a44ee1
Arian Rezazadeh <arianrezazadehs3@gmail.com>
2021-04-29 15:08:26 +0430

    complete donation section ui

--- src/contents/donate.tsx
+++ src/contents/donate.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useSelector } from 'react-redux';
-
+import {ReactComponent as HeartPic} from '../images/heart.svg';
 
 function Donate(): JSX.Element{
   const donators = useSelector((state: RootReducer) => state.donators);
@@ -8,16 +8,18 @@ function Donate(): JSX.Element{
   const extraClass = animIsLoaded ? "inner-hover": "animate__animated animate__zoomIn";
 
   useEffect(() => {
-    setTimeout(() => {
+    const animTimeout = setTimeout(() => {
       setAnimload(true);
     }, 1500);
+
+    return () => {clearTimeout(animTimeout);}
   }, [setAnimload]);
 
   return (
     <div id="donate">
       <div id="donate-donate" className={`inner-box ${extraClass}`}>
         <div className="title">حمایت</div>
-        <div className="title-info" style={{flexGrow: 1}}>برای حمایت از ما می تونید مبلغ دلخواهی رو به ما اهدا کنید که به ما در تمدید و ارتقا سرور کمک می کنه همچنین برای قدردانی ازت رنک مخصوصی دریافت می کنی</div>
+        <div className="title-info" style={{flexGrow: 1}}>برای حمایت از ما می تونید مبلغ دلخواهی رو به ما اهدا کنید که به ما در تمدید و ارتقا سرور کمک می کنه</div>
         <div id="input-container">
           <input placeholder="مبلغ به تومان" type="number"/>
           <button style={{marginTop: ".25em"}}>ثبت</button>
@@ -26,17 +28,18 @@ function Donate(): JSX.Element{
       <div id="donate-donators" className={`inner-box ${extraClass} animate__delay-1s`}>
         <div className="title">حامیان مالی</div>
         <div id="donators">
-          {donators.map((val) => 
-            <div className="donator-layout">
+          {donators.map((val, index) => 
+            <div key={index} className="donator-layout">
               <div>{val.name}</div>
               <div>{val.amount}</div>
             </div>
           )}
-
         </div>
       </div>
       <div id="donate-price" className={`inner-box ${extraClass} animate__delay-2s`}>
         <div className="title">جوایز</div>
+        <div className="title-info">برای قدردانی، حامیان یک رنک مخصوصی دریافت می کنند</div>
+        <HeartPic width={64} height={64}/>
       </div>
     </div>
   )
